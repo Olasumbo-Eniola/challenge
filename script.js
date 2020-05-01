9061232bfb4ecf8763b795eb8ed5532e45fc6d96
Olasumbo-Eniola <olasumboeniola1@gmail.com>
2020-05-01 05:38:38 -0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ function validateForm(){
   outputs[1] = document.forms['form']['lname'].value;
   outputs[2] = document.forms['form']['email'].value;
   outputs[3] = document.forms['form']['password'].value;
-  let mail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  var mail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
   
 
   if(outputs[0] == null || outputs[0] == ''){
@@ -19,7 +20,7 @@ function validateForm(){
     document.getElementsByTagName('input')[1].style.border = '2px solid red';
     return false;
 
-  }else if (outputs[2] == mail ){
+  }else if (!outputs[2].match(mail)){
     document.getElementsByTagName('svg')[2].style.display = 'block';
     document.getElementsByTagName('small')[3].style.display = 'block';
     document.getElementsByTagName('input')[2].style.border = '2px solid red';
